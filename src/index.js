ab0694d5b9a0c02307f8e30c722bc05bca422cb2
tirth765 <tirth.patel6521@gmail.com>
2025-01-23 09:37:44 +0530

    file_http

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
-console.log('Hello Node!');
+require('dotenv').config()
+const express = require('express')
+
+const app = express()
+const port = 8000
+
+app.listen(8000, () => {
+    console.log(`Example app listening on port`)
+})
+
+app.get('/demo', (req, res) => {
+    res.send('get Method!')
+})
+
+app.post('/demo', (req, res) => {
+    res.send('post Method!')
+})
+
+// console.log('Hello Node!', process.env.PORT);
