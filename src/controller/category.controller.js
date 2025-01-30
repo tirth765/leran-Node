0ed6a1b48d569086db09d83a2e11cdd0bc2b583f
tirth765 <tirth.patel6521@gmail.com>
2025-01-30 10:38:34 +0530

    ok

--- src/controller/category.controller.js
+++ src/controller/category.controller.js
@@ -1,71 +1,72 @@
 const Categores = require("../models/category.model");
+const fs = require("fs");
 
 const listCategores = async (req, res) => {
- try {
-  const categores = await Categores.find()
-  res.json(categores)
-  if(!categores) {
-    return res.status(400) 
-    .json({
-      success:false,
-      data : null,
-      message: "Error"
-    })
+  try {
+    const categores = await Categores.find()
+    res.json(categores)
+    if (!categores) {
+      return res.status(400)
+        .json({
+          success: false,
+          data: null,
+          message: "Error"
+        })
+    }
+
+    return res.status(200)
+      .json({
+        success: true,
+        data: categores,
+        message: "All Categores List Succesfully"
+      })
+
+  } catch (error) {
+    return res.status(500)
+      .json({
+        success: false,
+        data: null,
+        message: "Internal server Error" + error.message
+      })
   }
-
-  return res.status(200) 
-  .json({
-    success:true,
-    data : categores,
-    message: "All Categores List Succesfully"
-  })
-
- } catch (error) {
-  return res.status(500) 
-  .json({
-    success:false,
-    data : null,
-    message: "Internal server Error" + error.message
-  })
- }
 };
 
 const getCategory = async (req, res) => {
   try {
-   const category = await Categores.findById(req.params.id)
-   res.json(category)
-   if(!category) {
-     return res.status(400) 
-     .json({
-       success:false,
-       data : null,
-       message: "Error"
-     })
-   }
- 
-   return res.status(200) 
-   .json({
-     success:true,
-     data : category,
-     message: "All Category List Succesfully"
-   })
- 
+    const category = await Categores.findById(req.params.id)
+    res.json(category)
+    if (!category) {
+      return res.status(400)
+        .json({
+          success: false,
+          data: null,
+          message: "Error"
+        })
+    }
+
+    return res.status(200)
+      .json({
+        success: true,
+        data: category,
+        message: "All Category List Succesfully"
+      })
+
   } catch (error) {
-   return res.status(500) 
-   .json({
-     success:false,
-     data : null,
-     message: "Internal server Error" + error.message
-   })
+    return res.status(500)
+      .json({
+        success: false,
+        data: null,
+        message: "Internal server Error" + error.message
+      })
   }
- };
+};
 
 const addCategory = async (req, res) => {
   try {
 
-    console.log("hi ",req.body);
+    console.log("hi ", req.body);
 
-    const category = await Categores.create({...req.body, cat_img: req.file.path})
+    const category = await Categores.create({ ...req.body, cat_img: req.file.path })
 
     if (!category) {
       return res.status(400)
@@ -92,26 +93,50 @@ const addCategory = async (req, res) => {
   }
 };
 
-const updateCategory = async(req, res) => {
+const updateCategory = async (req, res) => {
   console.log(req.params.id);
-  
-  try {
-    const category = await Categores.findByIdAndUpdate(req.params.id, req.body)
 
-    if (!category) {
-      return res.status(400)
+  try {
+
+    if (req.file) {
+      const oldcategory = await Categores.findById(req.params.id, { ...req.body }, {
+        new: true,
+        runValidators: true
+      })
+
+      fs.unlink(oldcategory.cat_img, req.file.path, (err) => {
+        if (err) {
+          return res.status(400)
+            .json({
+              success: false,
+              data: [],
+              message: "Error" 
+            })
+        }
+
+      })
+    } else {
+      const category = await Categores.findByIdAndUpdate(req.params.id, { ...req.body }, {
+        new: true,
+        runValidators: true
+      })
+
+      if (!category) {
+        return res.status(400)
+          .json({
+            success: false,
+            data: [],
+            message: "Error"
+          })
+      }
+      return res.status(200)
         .json({
-          success: false,
-          data: [],
-          message: "Error"
+          success: true,
+          data: category,
+          message: "category Updated Succesfully"
         })
     }
-    return res.status(200)
-      .json({
-        success: true,
-        data: category,
-        message: "category Updated Succesfully"
-      })
+
 
   } catch (error) {
     return res.status(500)
@@ -123,9 +148,13 @@ const updateCategory = async(req, res) => {
   }
 };
 
-const deleteCategory = async(req, res) => {
+const deleteCategory = async (req, res) => {
+  console.log(req.params.id);
+  
   try {
     const category = await Categores.findByIdAndDelete(req.params.id)
+
+
 
     if (!category) {
       return res.status(400)
@@ -135,6 +164,17 @@ const deleteCategory = async(req, res) => {
           message: "Error"
         })
     }
+
+    fs.unlink(category.cat_img, (err) => {
+      return res.status(400)
+        .json({
+          success: false,
+          data: null,
+          message: "Error"
+        })
+    })
+
+
     return res.status(200)
       .json({
         success: true,
